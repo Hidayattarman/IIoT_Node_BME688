b9d50c45bd723b10628ee21129835645023e7def
Hidayattarman <armancirebon2019@gmail.com>
2026-06-25 12:08:07 +0700

    nambahin pw wifi doang

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 
-// ================= KONFIGURASI =================
-const char* ssid = "NAMA_WIFI_KAMU";        
-const char* password = "PASSWORD_WIFI_KAMU"; 
-const char* mqtt_server = "IP_ADDRESS_PC_KAMU"; // Contoh: "192.168.1.5"
+const char* ssid = "ArmanHidayat";        
+const char* password = "123ahjk"; 
+const char* mqtt_server = "1.3456.76.88";
 
 #define I2C_SDA 21
 #define I2C_SCL 22
@@ -46,7 +45,6 @@ void reconnect() {
 void setup() {
   Serial.begin(115200);
   
-  // Inisialisasi I2C dan Sensor
   Wire.begin(I2C_SDA, I2C_SCL);
   if (!bme.begin(BME688_ADDR)) { 
     Serial.println("BME688 tidak ditemukan! Cek kabel.");
@@ -54,7 +52,6 @@ void setup() {
   }
   Serial.println("BME688 Siap!");
   
-  // Setting default BME688
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
@@ -71,7 +68,6 @@ void loop() {
   }
   client.loop();
 
-  // Kirim data setiap 5 detik
   if (millis() - lastMsg > 5000) {
     lastMsg = millis();
 
@@ -80,7 +76,6 @@ void loop() {
       return;
     }
 
-    // Buat format JSON
     char payload[150];
     snprintf(payload, sizeof(payload), 
              "{\"temp\":%.2f, \"hum\":%.2f, \"press\":%.2f, \"gas\":%.2f}", 
@@ -89,7 +84,6 @@ void loop() {
     Serial.print("Kirim data: ");
     Serial.println(payload);
 
-    // Publish ke broker
     client.publish("iiot/test/bme688", payload);
   }
 }
